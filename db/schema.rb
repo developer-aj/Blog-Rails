1af39bd6a53a8a41035ca744b2e8896f16549041
Ajay Yadav <ajju.kvmc@gmail.com>
2014-09-02 01:38:20 +0530

    adding model, view and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901134118) do
+ActiveRecord::Schema.define(version: 20140901191028) do
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
